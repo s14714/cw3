0515edc5177fc9b75f646a6291b03489a0a407b7
s14714 <s14714@pjwstk.edu.pl>
2020-03-29 17:51:12 +0200

    zrobione

---
+++ cw3/cw3/Controllers/StudentsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cw3.DAL;
+using cw3.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw3.Controllers
+{
+    
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+        [HttpGet]
+
+        public IActionResult GetStudent(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+        //public string GetStudent(string orderBy)
+        //{
+        //    return $"Kowalski, Malewski, Andrzejewski sortowanie={orderBy}";
+        //}
+
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+            else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Nie znaleziono Studenta");
+        }
+        public String getStudent()
+        {
+            return "Kowalski, Malewski, Andrzejewski";
+        }
+
+    }
+
+
+}
